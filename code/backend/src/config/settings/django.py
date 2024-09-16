6f5745f19953bb59da44c5ecea9225e86dea1594
Yury Marozau <ymoroz.09@gmail.com>
2024-09-16 22:15:28 +0300

    CCIO-6. Added staticfiles app to serve static for admin/docs.

--- code/backend/src/config/settings/django.py
+++ code/backend/src/config/settings/django.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.staticfiles',
     'django.contrib.messages',
 
     # Project apps
