643b661f54d2e93b76614479af8b8056650a24a8
Yury Marozau <ymoroz.09@gmail.com>
2024-10-02 22:26:56 +0300

    CCIO-6. Moved AppKit creation to the module with config creation.

--- code/frontend/src/components/topbar/WalletConnectAppKit.tsx
+++ code/frontend/src/components/topbar/WalletConnectAppKit.tsx
@@ -1,32 +1,5 @@
 import { Stack } from '@mui/material';
 
-import { createAppKit } from '@reown/appkit/react';
-import { createSIWE } from 'components/topbar/siwe';
-
-import { metadata, networks, wagmiAdapter } from 'providers/WagmiContextProvider';
-
-const projectId = import.meta.env.FE_WALLETCONNECT_PROJECT_ID;
-
-const siweConfig = createSIWE(networks.map(network => network.id) as [number]);
-
-createAppKit({
-    adapters: [wagmiAdapter],
-    projectId: projectId,
-    networks: networks,
-    metadata: metadata,
-    allWallets: 'ONLY_MOBILE',
-    features: {
-        swaps: false,
-        onramp: false,
-        email: false,
-        socials: false,
-    },
-    siweConfig: siweConfig,
-    enableWalletConnect: false,
-    enableInjected: false,
-    enableCoinbase: false,
-});
-
 export default function WalletConnectAppKit() {
     return (
         <Stack
--- code/frontend/src/providers/WagmiContextProvider.tsx
+++ code/frontend/src/providers/WagmiContextProvider.tsx
@@ -1,44 +1,65 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { baseSepolia, sepolia } from '@reown/appkit/networks'
+import { arbitrum, baseSepolia, sepolia } from '@reown/appkit/networks'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
+import { injected, walletConnect } from 'wagmi/connectors';
+
+import { createAppKit } from '@reown/appkit/react';
+import { createSIWE } from 'components/topbar/siwe';
 
 const queryClient = new QueryClient();
 const projectId = import.meta.env.FE_WALLETCONNECT_PROJECT_ID;
 
-export const metadata = {
+const metadata = {
     name: 'CCIO',
     description: 'CCIO - Crypto Chart IO',
     url: 'ccio',
     icons: [],
 };
 
-export const networks = [baseSepolia, sepolia]
+const networks = [arbitrum, baseSepolia, sepolia];
 
-export const wagmiAdapter = new WagmiAdapter({
+const wagmiAdapter = new WagmiAdapter({
     networks: networks,
     projectId: projectId,
-    // transports: {
-    //     [sepolia.id]: http(),
-    // },
-    // connectors: [
-    //     injected({
-    //         shimDisconnect: false
-    //     }),
-    //     walletConnect({
-    //         projectId,
-    //         metadata,
-    //         showQrModal: true
-    //     }),
-    // ],
+    connectors: [
+        injected({
+            shimDisconnect: true
+        }),
+        walletConnect({
+            projectId,
+            metadata,
+            showQrModal: true
+        }),
+    ],
 });
 
+const siweConfig = createSIWE(networks.map(network => network.id) as [number]);
+
+createAppKit({
+    debug: true,
+    adapters: [wagmiAdapter],
+    projectId: projectId,
+    networks: networks,
+    metadata: metadata,
+    allWallets: 'ONLY_MOBILE',
+    features: {
+        swaps: false,
+        onramp: false,
+        email: false,
+        socials: false,
+    },
+    siweConfig: siweConfig,
+    enableWalletConnect: false,
+    enableInjected: false,
+    enableCoinbase: false,
+});
 
 export function WagmiContextProvider({children}) {
     return (
         <WagmiProvider
             config={wagmiAdapter.wagmiConfig}
-            reconnectOnMount={true}
+            reconnectOnMount={false}
         >
             <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
         </WagmiProvider>
